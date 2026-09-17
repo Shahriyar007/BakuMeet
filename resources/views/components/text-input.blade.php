f98870defcfb99ed208190f922e99983baad26c2
Shahriyar007 <eyvazovshahriyar@gmail.com>
2026-09-17 11:16:45 +0400

    fix: text-input component shows red border automatically when its field has a validation error

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,11 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {!! $attributes->merge(['class' => 'bk-input', 'style' => 'width: 100%; margin-bottom: 4px;']) !!}>
+@php
+    $fieldName = $attributes->get('name');
+    $hasError = $fieldName && $errors->has($fieldName);
+@endphp
+
+<input @disabled($disabled) {!! $attributes->merge([
+    'class' => 'bk-input',
+    'style' => 'width: 100%; margin-bottom: 4px;' . ($hasError ? ' border-color: var(--color-danger);' : ''),
+]) !!}>
